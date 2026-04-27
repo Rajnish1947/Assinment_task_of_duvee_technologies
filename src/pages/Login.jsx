21f9edc553dbbdfb263364d695853a9828ad01bc
Rajnish1947 <kumarrajnish28443@gmail.com>
2026-04-27 18:27:41 +0530

    input text color change

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -53,7 +53,7 @@ export default function Login() {
           <input
             type="email"
             placeholder="Email Address"
-            className="w-full px-4 py-2 rounded-lg bg-white/20 text-white placeholder-gray-300 outline-none focus:ring-2 focus:ring-sky-400"
+            className="w-full px-4 py-2 rounded-lg bg-white/20 text-black placeholder-gray-300 outline-none focus:ring-2 focus:ring-sky-400"
             onChange={(e) => setEmail(e.target.value)}
             required
           />
@@ -61,7 +61,7 @@ export default function Login() {
           <input
             type="password"
             placeholder="Password"
-            className="w-full px-4 py-2 rounded-lg bg-white/20 text-white placeholder-gray-300 outline-none focus:ring-2 focus:ring-sky-400"
+            className="w-full px-4 py-2 rounded-lg  text-black placeholder-gray-500 outline-none focus:ring-2 focus:ring-sky-400"
             onChange={(e) => setPassword(e.target.value)}
             required
           />
